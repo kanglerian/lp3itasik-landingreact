ecffc3827052e910eef55990ddd3c4e45f438fc1
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-05-11 11:21:25 +0700

    flowbite

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -32,7 +32,7 @@ const Banner = () => {
       {isLoaded ? (
         <div className="relative z-0">
           {banners.length > 0 ? (
-            <div className="relative h-56 overflow-hidden rounded-lg md:h-[550px]">
+            <div className="relative h-56 overflow-hidden rounded-lg md:h-screen">
               <OwlCarousel className='owl-theme' items={1} loop margin={10} autoplay>
                 {banners.map((banner, i) =>
                   <div className="item" key={i}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    'node_modules/flowbite/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
