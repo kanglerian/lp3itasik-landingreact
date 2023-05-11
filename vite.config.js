57972cbde5dce26ed689d02bf8e04c96d15d9de1
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-05-11 13:31:19 +0700

    edit base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://cobavite.politekniklp3i-tasikmalaya.ac.id/',
+  base: 'https://politekniklp3i-tasikmalaya.ac.id/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
