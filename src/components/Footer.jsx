490eaea33598002737cbaa1768818f3f7d24835b
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-05-23 09:20:51 +0700

    cek

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,12 +30,12 @@ const Footer = () => {
           <div>
             <h5 className="text-gray-700 font-bold text-left md:text-right text-xl">{currentLanguage == 'en' ? 'About Campus' : 'Tentang Kampus'}</h5>
             <ul className="text-gray-500 mt-4 text-xs text-left md:text-right space-y-2">
-              <li><a href="#">{currentLanguage == 'en' ? 'Digital Brochure' : 'Brosur Digital'}</a></li>
-              <li><a href="#">{currentLanguage == 'en' ? 'Get to know LP3I' : 'Mengenal LP3I'}</a></li>
-              <li><a href="#">{currentLanguage == 'en' ? 'History of LP3I' : 'Sejarah LP3I'}</a></li>
-              <li><a href="#">{currentLanguage == 'en' ? 'Vision & Mission' : 'Visi & Misi'}</a></li>
-              <li><a href="#">{currentLanguage == 'en' ? 'Organizational Structure' : 'Struktur Organisasi'}</a></li>
-              <li><a href="#">{currentLanguage == 'en' ? 'Campus Facilities' : 'Fasilitas Kampus'}</a></li>
+              <li><a href="https://brosur.politekniklp3i-tasikmalaya.ac.id/">{currentLanguage == 'en' ? 'Digital Brochure' : 'Brosur Digital'}</a></li>
+              <li><a href={`/about`}>{currentLanguage == 'en' ? 'Get to know LP3I' : 'Mengenal LP3I'}</a></li>
+              <li><a href={`/about`}>{currentLanguage == 'en' ? 'History of LP3I' : 'Sejarah LP3I'}</a></li>
+              <li><a href={`/about`}>{currentLanguage == 'en' ? 'Vision & Mission' : 'Visi & Misi'}</a></li>
+              <li><a href={`/about`}>{currentLanguage == 'en' ? 'Organizational Structure' : 'Struktur Organisasi'}</a></li>
+              <li><a href={`/about`}>{currentLanguage == 'en' ? 'Campus Facilities' : 'Fasilitas Kampus'}</a></li>
             </ul>
           </div>
           <div>
