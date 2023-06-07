df48a298ca449a66f4552a6ffc065ae1feaff34c
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-06-07 10:30:18 +0700

    oi

--- src/components/Flyer.jsx
+++ src/components/Flyer.jsx
@@ -12,6 +12,10 @@ const Flyer = () => {
   const [success, setSuccess] = useState(false);
   const [failed, setFailed] = useState(false);
 
+  const [headline, setHeadline] = useState('');
+  const [paragraph, setParagraph] = useState('');
+  const [image, setImage] = useState('');
+
   const handleSend = async () => {
     await axios.post(`https://database.politekniklp3i-tasikmalaya.ac.id/api/storewebsite`,{
       name: name,
@@ -31,6 +35,19 @@ const Flyer = () => {
     })
   }
 
+  const getFlyer = async () => {
+    await axios.get(`https://dashboard.politekniklp3i-tasikmalaya.ac.id/api/flyers`)
+    .then((res) => {
+      let flyers = res.data;
+      setHeadline(flyers[0].headline);
+      setParagraph(flyers[0].paragraph);
+      setImage(flyers[0].image);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    })
+  }
+
   const handleVisible = () => {
     setVisible(!isVisible);
   }
@@ -47,23 +64,22 @@ const Flyer = () => {
       once: true
     });
     AOS.refresh();
-    // loadingModal();
+    getFlyer();
     setTimeout(loadingModal, 3000);
   }, []);
 
   return (
     <>
-      {isVisible &&
+      {isVisible && (
         <div id="myModal" className="fixed inset-0 flex items-center justify-center z-50">
           <div className="bg-gray-900 bg-opacity-75 flex items-center justify-center h-screen w-full px-5">
             <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1000" className="w-3/3 md:w-2/3 bg-slate-100 rounded-xl p-5 max-h-screen overflow-y-auto">
               <div className='relative flex justify-center items-center flex-col md:flex-row gap-5'>
                 <button onClick={handleVisible} className='absolute top-0 right-0'><i className="fa-solid fa-circle-xmark fa-1x"></i></button>
-                <div className="w-full md:w-1/2 bg-cover bg-center h-[200px] md:h-[450px] rounded-xl" style={{ backgroundImage: `url(${imageFlyer})` }}></div>
+                <div className="w-full md:w-1/2 bg-cover bg-center h-[200px] md:h-[450px] rounded-xl" style={{ backgroundImage: `url('https://dashboard.politekniklp3i-tasikmalaya.ac.id/${image}')` }}></div>
                 <div className='w-full md:w-1/2 space-y-2'>
-                  <h1 className='text-2xl font-bold' data-aos="fade-up" data-aos-delay="1200">Daftar di Politeknik LP3I Kampus Tasikmalaya Sekarang, Dapatkan Beasiswa hingga Rp5.000.000.</h1>
-                  <p data-aos="fade-up" data-aos-delay="1300">Jangan lewatkan penawaran terbatas ya sahabat.</p>
-                  <h4 className='text-sm' data-aos="fade-up" data-aos-delay="1100">MASIH BINGUNG CARI KAMPUS?</h4>
+                  <h1 className='text-2xl font-bold' data-aos="fade-up" data-aos-delay="1200">{headline}</h1>
+                  <p data-aos="fade-up" data-aos-delay="1300">{paragraph}</p>
                   {
                     success && (
                       <div className='bg-emerald-500 py-1 px-3 text-white rounded-lg'>
@@ -87,7 +103,7 @@ const Flyer = () => {
             </div>
           </div>
         </div>
-      }
+      ) }
     </>
   )
 }
