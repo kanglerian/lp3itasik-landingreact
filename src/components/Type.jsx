862cf0bb60dfb873a633cce361bc23c4c339a7b0
kanglerian <kanglerian@gmail.com>
2023-08-14 10:03:28 +0700

    tipe desc

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -9,44 +9,48 @@ const Type = () => {
             <h1 className='text-2xl font-bold text-gray-900'>Informasi Penerimaan Mahasiswa Baru</h1>
             <p className='text-gray-700' data-aos-delay="100">Politeknik LP3I Kampus Tasikmalaya juga menerima mahasiswa:</p>
           </header>
-          <div className="flex flex-wrap justify-center items-center">
-            <div className='w-1/2 md:w-1/4 p-5' data-aos-delay="200">
+          <div className="flex flex-wrap justify-center items-center text-center">
+            <div className='w-full md:w-1/4 p-5' data-aos-delay="200">
               <div className='bg-white border border-gray-100 rounded-xl p-5'>
                 <div className='space-y-5'>
                   <div className='flex flex-col items-center gap-3'>
                     <i class="fa-solid fa-repeat fa-2x text-gray-900"></i>
-                    <h3 className='font-bold text-2xl text-gray-900'>Transfer Kredit</h3>
+                    <h3 className='font-bold text-xl text-gray-900'>Transfer Kredit</h3>
                   </div>
+                  <p className='text-sm text-gray-700'>Kembangkan potensimu di LP3I dengan mudah! Transfer kredit dari perguruan tinggi lain, raih kesuksesan kariermu bersama kami.</p>
                 </div>
               </div>
             </div>
-            <div className='w-1/2 md:w-1/4 p-5' data-aos-delay="200">
+            <div className='w-full md:w-1/4 p-5' data-aos-delay="200">
               <div className='bg-white border border-gray-100 rounded-xl p-5'>
                 <div className='space-y-5'>
                   <div className='flex flex-col items-center gap-3'>
                     <i class="fa-solid fa-wheelchair fa-2x text-gray-900"></i>
-                    <h3 className='font-bold text-2xl text-gray-900'>Disabilitas</h3>
+                    <h3 className='font-bold text-xl text-gray-900'>Disabilitas</h3>
                   </div>
+                  <p className='text-sm text-gray-700'>Jembatani impian dengan inklusi. Kami menyambut mahasiswa disabilitas untuk meraih pendidikan berkualitas dengan dukungan dan inspirasi penuh.</p>
                 </div>
               </div>
             </div>
-            <div className='w-1/2 md:w-1/4 p-5' data-aos-delay="200">
+            <div className='w-full md:w-1/4 p-5' data-aos-delay="200">
               <div className='bg-white border border-gray-100 rounded-xl p-5'>
                 <div className='space-y-5'>
                   <div className='flex flex-col items-center gap-3'>
                     <i class="fa-regular fa-building fa-2x text-gray-900"></i>
-                    <h3 className='font-bold text-2xl text-gray-900'>Mahasiswa Pindahan</h3>
+                    <h3 className='font-bold text-xl text-gray-900'>Mahasiswa Pindahan</h3>
                   </div>
+                  <p className='text-sm text-gray-700'>Langkah baru, peluang tak terbatas. Bergabunglah sebagai mahasiswa pindahan, wujudkan prestasimu di lingkungan inspiratif kami.</p>
                 </div>
               </div>
             </div>
-            <div className='w-1/2 md:w-1/4 p-5' data-aos-delay="200">
+            <div className='w-full md:w-1/4 p-5' data-aos-delay="200">
               <div className='bg-white border border-gray-100 rounded-xl p-5'>
                 <div className='space-y-5'>
                   <div className='flex flex-col items-center gap-3'>
                     <i class="fa-solid fa-earth-asia fa-2x text-gray-900"></i>
-                    <h3 className='font-bold text-2xl text-gray-900'>Warga Negara Asing</h3>
+                    <h3 className='font-bold text-xl text-gray-900'>Warga Negara Asing</h3>
                   </div>
+                  <p className='text-sm text-gray-700'>Global dreams start here! Join us, international students, for world-class education, cultural diversity, and boundless opportunities.</p>
                 </div>
               </div>
             </div>
