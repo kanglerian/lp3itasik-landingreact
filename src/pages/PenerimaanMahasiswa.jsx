8fd3df7aea90f053ae0aa598b4f9dc7204d5ceae
kanglerian <kanglerian@gmail.com>
2023-09-06 11:30:01 +0700

    redirect

--- src/pages/PenerimaanMahasiswa.jsx
+++ src/pages/PenerimaanMahasiswa.jsx
@@ -13,8 +13,8 @@ const PenerimaanMahasiswa = () => {
     }, 2000);
   }, []);
   return (
-    <div className='container px-4 bg-gray-100'>
-      <div className='max-w-lg mx-auto flex flex-col h-screen justify-center items-center space-y-5'>
+    <div className='bg-gray-100'>
+      <div className='container px-4 max-w-lg mx-auto flex flex-col h-screen justify-center items-center space-y-5'>
         <img src={Logo} width={300} alt="Logo Politeknik LP3I Kampus Tasikmalaya" loading='lazy' />
         <div className='flex flex-col justify-center items-center text-center' style={{ marginTop: '-50px' }}>
           <Lottie animationData={LoadingAnimation} style={{ width: 220, height: 220 }} loading='lazy' />
